8993165b37bb62c1dd091062761c302e01c95800
MuhannadMutawe <muhannadrm208@gmail.com>
2026-07-21 16:43:26 +0300

    create Weekly List Component widget and add it to home view.

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,10 +1,19 @@
 import 'package:fitness_app_ui/app/designe/designe_token.dart';
 import 'package:fitness_app_ui/features/home/presentation/view/widgets/app_bar_title.dart';
 import 'package:fitness_app_ui/features/home/presentation/view/widgets/daily_challenge_card.dart';
+import 'package:fitness_app_ui/features/home/presentation/view/widgets/weekly_list_component.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +31,13 @@ class HomeView extends StatelessWidget {
         child: Column(
           children: [
             DailyChallengeCard(),
+            SizedBox(height: DT.s6),
+            WeeklyListComponent(
+              selectedDate: selectedDate,
+              onDateSelected: (date) => setState(() {
+                selectedDate = date;
+              }),
+            ),
           ],
         ),
       ),
---
+++ lib/features/home/presentation/view/widgets/weekly_list_component.dart
@@ -0,0 +1,73 @@
+import 'package:fitness_app_ui/app/designe/designe_token.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class WeeklyListComponent extends StatelessWidget {
+  const WeeklyListComponent({
+    super.key,
+    required this.selectedDate,
+    required this.onDateSelected,
+  });
+
+  final DateTime selectedDate;
+  final Function(DateTime date) onDateSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final startOfWeek = now.subtract(Duration(days: now.weekday - 1));
+    return SizedBox(
+      height: 70,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 7,
+        itemBuilder: (context, index) {
+          final date = startOfWeek.add(Duration(days: index));
+          final isSelected =
+              date.day == selectedDate.day &&
+              date.month == selectedDate.month &&
+              date.year == selectedDate.year;
+          return GestureDetector(
+            onTap: () => onDateSelected(date),
+            child: Container(
+              width: 50,
+              margin: EdgeInsets.only(right: DT.s3),
+              decoration: BoxDecoration(
+                color: isSelected ? DT.bgBlack : DT.bgWhite,
+                borderRadius: BorderRadius.circular(DT.rCardSmall),
+                boxShadow: [
+                  BoxShadow(
+                    color: DT.shadowMedium,
+                    blurRadius: 12,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    DateFormat('E').format(date),
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: isSelected ? DT.textWhite : DT.textPrimary,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d').format(date),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: isSelected ? DT.textWhite : DT.textPrimary,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

9d08fb504e94a80c09dc84fd15b82cf10f54ea5d
MuhannadMutawe <muhannadrm208@gmail.com>
2026-07-27 21:53:45 +0300

    create app bar for profile view.

--- lib/features/profile/presentation/view/profile_view.dart
+++ lib/features/profile/presentation/view/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app_ui/app/designe/designe_token.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,6 +6,37 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: DT.bgWhite,
+      appBar: AppBar(
+        backgroundColor: DT.bgWhite,
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            fontSize: 18,
+            color: DT.textPrimary,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: DT.textPrimary,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(DT.s5),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
